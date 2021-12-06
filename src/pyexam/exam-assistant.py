0982578e94c9a78b010da016d76ec1c292032db8
monkeychen <cza55007@gmail.com>
2021-12-06 21:27:58 +0800

    python自动评卷功能：50%

--- src/pyexam/exam-assistant.py
+++ src/pyexam/exam-assistant.py
@@ -2,19 +2,22 @@ import os
 import argparse
 import shutil
 from datetime import datetime
-import datetimeutils
 from openpyxl import load_workbook
 from unrar import rarfile
+import tkinter as tk
+
+
+FMT_DEFAULT = "%Y-%m-%d %H:%M:%S"
 
 
 class Answer(object):
-    score_conf = {
-        "single_choice": 2,
-        "multi_choice": 2,
-        "judgment": 1,
-        "programme_fill": 3,
-        "complex": 3
-    }
+    score_conf = [
+        {"title": "single_choice", "unit": 2},
+        {"title": "multi_choice", "unit": 3},
+        {"title": "judgment", "unit": 2},
+        {"title": "programme_fill", "unit": 5},
+        {"title": "complex", "unit": 10},
+    ]
 
     def __init__(self, single_choice=None, multi_choice=None, judgment=None):
         self.single_choice = single_choice
@@ -26,8 +29,7 @@ class Answer(object):
 
 class Student(object):
 
-    def __init__(self, sid, name, answer: Answer = None, std_answer: Answer = None, valid=True):
-        self.sid = sid
+    def __init__(self, name, answer: Answer = None, std_answer: Answer = None, valid=True):
         self.name = name
         self.answer = answer
         self.score = 0
@@ -39,21 +41,32 @@ class Student(object):
         if self.answer is None:
             self.valid = False
             return
-        for q_type in Answer.score_conf.keys():
+        for conf in Answer.score_conf:
+            unit = conf.get("unit")
+            q_type = conf.get("title")
             tmp_std_ans_values = getattr(self.std_answer, q_type)
             tmp_ans_values = getattr(self.answer, q_type)
             if len(tmp_ans_values) != len(tmp_std_ans_values):
                 self.valid = False
                 return
             for i in range(0, len(tmp_std_ans_values)):
-                # TODO
-                pass
+                self.score = self.score + (unit if tmp_std_ans_values[i] == tmp_ans_values[i] else 0)
+        self.valid = self.score < 20
+        return self.score
 
 
 class ExamAssistant(object):
     root_dir = os.getcwd()
+    question_conf = [
+        {"begin": "程序填空题01", "end": "txt"},
+        {"begin": "程序填空题02", "end": "txt"},
+        {"begin": "程序填空题03", "end": "txt"},
+        {"begin": "综合题01", "end": "txt"},
+        {"begin": "综合题02", "end": "txt"}
+    ]
 
-    def __init__(self):
+    def __init__(self, tk_inst: tk.Tk):
+        self.tk_inst = tk_inst
         self.args = self.parse_cli_args()
         self.root_dir = self.args.root_dir
         self.answer_file_num = self.args.answer_file_num
@@ -63,6 +76,23 @@ class ExamAssistant(object):
         self.action = self.args.action
         self.std_answer = self.load_answer_file(f"{self.answer_dir}/答题卡.xlsx")
         self.student_list = []
+        self.init_win_layout()
+
+    def init_win_layout(self):
+        self.tk_inst.title("压缩包检测工具V1.0")
+        self.tk_inst.geometry('900x600')
+
+        self.btn = tk.Button(self.tk_inst, text="压缩包内容检测", bg="lightblue", width=30, height=1, command=self.execute)
+        self.btn.pack(pady=15)
+        self.label3 = tk.Label(self.tk_inst, text="本产品只获取当前文件夹中rar压缩包的总数，\n以及检测rar中是否包含Acopr与prog、答题卡xlsx文件。\n如需更详细的检测，请进行人工判定")
+        self.label3.pack(pady=1)
+        self.text2 = tk.StringVar()
+        self.label2 = tk.Label(self.tk_inst, textvariable=self.text2, fg="red", width=90, relief="sunken", bg="white", font=16)
+        self.label2.pack(pady=1)
+
+        self.text1 = tk.StringVar()
+        self.label1 = tk.Label(self.tk_inst, textvariable=self.text1, fg="red", width=90, height=30, relief="sunken", bg="white", font=16)
+        self.label1.pack()
 
     def load_answer_file(self, answer_file_path):
         answer_card_wb = load_workbook(answer_file_path)
@@ -84,42 +114,65 @@ class ExamAssistant(object):
         return answer
 
     def submit_check(self):
-        rar_list = os.listdir(self.upload_dir)
         self._remove_dir_tree(dir_path=self.temp_dir, include_self=False)
-
-        for rar_name in rar_list:
-            if not rar_name.endswith("rar") and not rar_name.endswith("zip"):
-                print(f"The file [{rar_name}] doesn't end with rar and zip, so ignored it!")
+        dir_list = os.listdir(self.upload_dir)
+        check_report = []
+        student_list = []
+        for stu_main_dir_name in dir_list:
+            stu_main_dir_path = f"{self.upload_dir}/{stu_main_dir_name}"
+            if "temp" == stu_main_dir_name or os.path.isfile(stu_main_dir_path) or len(os.listdir(stu_main_dir_path)) == 0:
+                print(f"{stu_main_dir_path} is file or is an empty directory or a temp dir!")
                 continue
-            rar_file_path = f"{self.upload_dir}/{rar_name}"
-            s_id = rar_name[0:9]
-            s_name = rar_name[9:-4]
-            unrar_tmp_dir_path = f"{self.temp_dir}/{s_id}_{s_name}"
-            student = Student(sid=s_id, name=s_name, std_answer=self.std_answer)
-            is_valid = True
-            try:
-                self._remove_dir_tree(dir_path=unrar_tmp_dir_path, include_self=False)
-                rar_file = rarfile.RarFile(rar_file_path)
-                rar_file.extractall(path=unrar_tmp_dir_path)
-                tmp_stu_dir_path = f"{unrar_tmp_dir_path}/{s_id}{s_name}"
-                student.answer_dir_path = tmp_stu_dir_path
-                card_path = f"{tmp_stu_dir_path}/答题卡.xlsx"
-                if os.path.exists(card_path):
-                    answer = self.load_answer_file(card_path)
-                    student.answer = answer
-                else:
-                    is_valid = False
-                if is_valid:
-                    code_file_list = self._list_dir(dir_path=tmp_stu_dir_path, ext=self.args.ext_name)
-                    is_valid = len(code_file_list) == self.answer_file_num
-                student.valid = is_valid
-                student.calc_score()
-            except Exception as e:
-                print(f"Fail to parse student[{s_id}: {s_name}]'s answer rar file!")
+            student = Student(name=stu_main_dir_name, std_answer=self.std_answer)
+            student_list.append(student)
+            rar_cnt = 0
+            rar_file_path = None
+            for stu_file_name in os.listdir(stu_main_dir_path):
+                if not stu_file_name.endswith("rar") and not stu_file_name.endswith("zip"):
+                    continue
+                rar_cnt += 1
+                if rar_cnt > 1:
+                    check_report.append(f"目录【{stu_main_dir_name}]下发现多份RAR文件，请人工确认是否异常！")
+                    student.valid = False
+                    break
+                rar_file_path = f"{stu_main_dir_path}/{stu_file_name}"
 
-        invalid_student_list = []
+            if student.valid:
+                unrar_tmp_dir_path = f"{self.temp_dir}/{stu_main_dir_name}"
+                try:
+                    self._remove_dir_tree(dir_path=unrar_tmp_dir_path, include_self=False)
+                    rar_file = rarfile.RarFile(rar_file_path)
+                    matched_name_list = []
+                    card_name = None
+                    for name_in_rar in rar_file.namelist():
+                        base_name = os.path.basename(name_in_rar)
+                        if self._is_matched_file(base_name):
+                            matched_name_list.append(name_in_rar)
+                        elif base_name == "答题卡.xlsx":
+                            card_name = name_in_rar
 
-        return invalid_student_list
+                    if len(matched_name_list) < self.answer_file_num or card_name is None:
+                        check_report.append(f"压缩文件【{rar_file_path}]下的文件数量不满意要求，请人工确认！")
+                        student.valid = False
+                        continue
+                    if student.valid:
+                        rar_file.extractall(path=unrar_tmp_dir_path)
+                        card_path = f"{unrar_tmp_dir_path}/{card_name}"
+                        student.answer_dir_path = os.path.dirname(card_path)
+                        if os.path.exists(card_path):
+                            student.answer = self.load_answer_file(card_path)
+                            student.calc_score()
+                        else:
+                            check_report.append(f"无法加载答题卡文件【{card_path}】，请人工确认！")
+                            student.valid = False
+                except Exception as e:
+                    print(f"Fail to parse student[{stu_main_dir_name}]'s answer rar file!")
+
+        invalid_student_list = [stu for stu in student_list if not stu.valid]
+        for stu in invalid_student_list:
+            if stu.score < 20:
+                check_report.append(f"学生【{stu.name}】的成绩为【{stu.score}】，低于20分，请人工确认答卷是否异常！")
+        return check_report
 
     def evaluate(self):
         pass
@@ -140,28 +193,45 @@ class ExamAssistant(object):
                     name_list.append(dir_name)
         return name_list
 
+    def _is_matched_file(self, file_name: str):
+        matched = False
+        for conf in self.question_conf:
+            matched = file_name.startswith(conf.get("begin")) and file_name.endswith(conf.get("begin"))
+            if matched:
+                break
+        return matched
+
+    def datetime_to_str(self, dt, fmt=FMT_DEFAULT):
+        return dt.strftime(fmt)
+
     def parse_cli_args(self):
         cli_parser = argparse.ArgumentParser(description="这是一个Python考试辅助工具。", add_help=False)
         cli_parser.add_argument('-a', '--action', nargs='?', choices=['submit', 'evaluate'], default='submit', help='设置运行环境（submit：交卷检测，evaluate：评卷），默认为：交卷检测')
         cli_parser.add_argument('-e', '--env_mode', nargs='?', choices=['dev', 'prod'], default='prod', help='设置运行环境（dev：开发环境，prod：生产环境），默认为：开发环境')
-        cli_parser.add_argument('-n', '--answer_file_num', nargs='?', type=int, default=3, help='学生提交的python文件数，默认为3。')
+        cli_parser.add_argument('-n', '--answer_file_num', nargs='?', type=int, default=5, help='学生提交的python文件数，默认为5。')
         cli_parser.add_argument('-r', '--root_dir', nargs='?', default=f"{os.getcwd()}", help='程序工作根目录路径。')
         cli_parser.add_argument('-x', '--ext_name', nargs='?', default="py", help='学生提交作业中程序文件扩展名，默认为：py')
         cli_parser.add_argument('-h', '--help', action='help', help='显示本帮助信息并退出')
         cli_parser.add_argument('-v', '--version', action='version', version='%(prog)s V0.0.1', help='显示当前版本信息并退出')
 
         args = cli_parser.parse_args()
-        scheduled_time = datetimeutils.datetime_to_str(datetime.now(), fmt=datetimeutils.FMT_DEFAULT)
+        scheduled_time = self.datetime_to_str(datetime.now(), fmt=FMT_DEFAULT)
         print(f"The programme[{cli_parser.prog}] was scheduled at {scheduled_time}, cwd = {os.getcwd()}, env_mode = {args.env_mode}, "
               f"root_dir = {args.root_dir}, answer_file_num = {args.answer_file_num}")
         return args
 
     def execute(self):
         if self.action == 'submit':
-            self.submit_check()
+            check_result = self.submit_check()
+            infos = "\n".join(check_result)
+            self.text1.set(infos)
+            for info in check_result:
+                print(info)
         else:
             self.evaluate()
 
 
 if __name__ == "__main__":
-    ExamAssistant().execute()
+    win = tk.Tk()
+    GUI = ExamAssistant(win)
+    win.mainloop()
--- src/pyexam/rarJudge.py
+++ src/pyexam/rarJudge.py
@@ -10,7 +10,7 @@ class WinFileChecker(object):
     def __init__(self, tk_inst: tk.Tk):
         self.tk_inst = tk_inst
         self.app_path = os.path.dirname(sys.argv[0])
-        #self.check_output = self.app_path + "/invalid_list.txt"
+        # self.check_output = self.app_path + "/invalid_list.txt"
         self.init_win_layout()
 
     def init_win_layout(self):
@@ -19,31 +19,31 @@ class WinFileChecker(object):
 
         self.btn = tk.Button(self.tk_inst, text="压缩包内容检测", bg="lightblue", width=30, height=1, command=self.rar_check)
         self.btn.pack(pady=15)
-        self.label3= tk.Label(self.tk_inst, text="本产品只获取当前文件夹中rar压缩包的总数，\n以及检测rar中是否包含Acopr与prog、答题卡xlsx文件。\n如需更详细的检测，请进行人工判定")
+        self.label3 = tk.Label(self.tk_inst, text="本产品只获取当前文件夹中rar压缩包的总数，\n以及检测rar中是否包含Acopr与prog、答题卡xlsx文件。\n如需更详细的检测，请进行人工判定")
         self.label3.pack(pady=1)
-        self.text2=tk.StringVar()
-        self.label2=tk.Label(self.tk_inst,textvariable=self.text2,fg="red",width=50,relief="sunken",bg="white",font=16)
+        self.text2 = tk.StringVar()
+        self.label2 = tk.Label(self.tk_inst, textvariable=self.text2, fg="red", width=50, relief="sunken", bg="white", font=16)
         self.label2.pack(pady=1)
 
-        self.text1=tk.StringVar()
-        self.label1=tk.Label(self.tk_inst,textvariable=self.text1,fg="red",width=50,height=30,relief="sunken",bg="white",font=16)
+        self.text1 = tk.StringVar()
+        self.label1 = tk.Label(self.tk_inst, textvariable=self.text1, fg="red", width=50, height=30, relief="sunken", bg="white", font=16)
         self.label1.pack()
 
     def rar_check(self):
-        str=""
-        num=0
+        str = ""
+        num = 0
         for rar_file in os.listdir(self.app_path):
             if rar_file.endswith("rar"):
                 self.is_rar_filename(self.app_path + "/" + rar_file)
-                num=num+1
+                num = num + 1
                 if self.is_rar_invalid(self.app_path + "/" + rar_file):
-                    str=str+rar_file + "  内容有误，请重新提交\n"
+                    str = str + rar_file + "  内容有误，请重新提交\n"
                 self.text1.set(str)
         self.text2.set("当前文件夹中rar压缩包数量：{}个".format(num))
 
-        #msg.showinfo("提示", "收到rar压缩包总数：{}个".format(num))
+        # msg.showinfo("提示", "收到rar压缩包总数：{}个".format(num))
 
-    def is_rar_filename(self,rar_path):
+    def is_rar_filename(self, rar_path):
         rar = rarfile.RarFile(rar_path)
         # for file in rar.filename:
         #
@@ -58,9 +58,9 @@ class WinFileChecker(object):
             if (file_name.startswith("Acopr") or file_name.startswith("prog")) and file_name.endswith(".accdb"):
                 flags = flags + 1
             elif file_name.endswith("xlsx") and file_name.startswith("答题卡"):
-                flags=flags+1
+                flags = flags + 1
         print(flags)
-        return flags !=5
+        return flags != 5
 
 
 if __name__ == "__main__":
