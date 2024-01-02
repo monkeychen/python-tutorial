43ecd472d374c420f1e7d01a4e2b708d56e103a2
monkeychen <cza55007@gmail.com>
2024-01-02 21:31:50 +0800

    pandas-groupby-demo

--- src/pandas01.py
+++ src/pandas01.py
@@ -3,18 +3,23 @@
 @Author: chenzhian
 @Time: 2023/11/30 11:51
 """
+import os
+
 import pandas as pd
 
 # 从Excel文件中读取数据
 tmp_dir = '/Users/chenzhian/workspace/python/tutorial/temp'
-file_path = f'{tmp_dir}/topo.xlsx'
-df: pd.DataFrame = pd.read_excel(file_path, sheet_name="合并", engine="openpyxl")
+file_path = f'{tmp_dir}/topo2.xlsx'
+df: pd.DataFrame = pd.read_excel(file_path, sheet_name="Sheet1", engine="openpyxl")
 
 df['C'] = df['C'].astype(str)
 # 使用groupby分组，并应用lambda函数来合并C列的值
 # result = df.groupby(['A', 'B']).agg({'C': '|'.join}).reset_index()
-result = df.groupby(['A', 'B'])['C'].agg('|'.join).reset_index()
+result = df.groupby(['ne_type', 'sc_type'])['measure_id'].agg('|'.join).reset_index()
 
-result.to_excel(f'{tmp_dir}/merge.xlsx', index=False, header=True)
+out_path = f"{tmp_dir}/merge2.xlsx"
+if os.path.exists(out_path):
+    os.remove(out_path)
+result.to_excel(out_path, index=True, header=True)
 
 
